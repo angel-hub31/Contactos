b51aafb8cd1ac627175ece432e269f41c9853250
angel-hub31 <moralesmales1@hotmail.com>
2026-04-24 20:06:33 -0500

    primer commit

---
+++ src/com/krakedev/contacto/entidades/Directorio.java
@@ -0,0 +1,36 @@
+package com.krakedev.contacto.entidades;
+
+import java.util.ArrayList;
+
+public class Directorio {
+ArrayList<Contacto> contactos;
+	
+
+	public Directorio(){
+		contactos=new ArrayList<Contacto>();
+
+	}
+
+	public ArrayList<Contacto> getContactos() {
+		return contactos;
+	}
+
+	public void setContactos(ArrayList<Contacto> contactos) {
+		this.contactos = contactos;
+	}
+	public void  agreagarContacto(Contacto contacto) {
+		contactos.add(contacto);
+		
+	}
+	public int obtenerCantidadContacto() {
+		int cantidad;
+		cantidad=contactos.size();	
+		return cantidad;
+	}
+	
+	
+
+}
+
+
+
---
+++ src/com/krakedev/contacto/entidades/test/TestAgregar.java
@@ -0,0 +1,20 @@
+package com.krakedev.contacto.entidades.test;
+
+import com.krakedev.contacto.entidades.Contacto;
+import com.krakedev.contacto.entidades.Directorio;
+
+public class TestAgregar {
+
+	public static void main(String[] args) {
+		Directorio dir=new Directorio();
+		
+		Contacto c1=new Contacto();
+		c1.setNombre("Ana");
+		dir.agreagarContacto(c1);
+	}
+
+
+
+	}
+
+
